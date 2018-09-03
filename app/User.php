394e7619755e21b22153d389cd7029ab0d11c23b
tusharghoshjoy <joy@romoni.xyz>
2018-09-03 16:24:32 +0600

    my first commit

---
+++ app/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use Notifiable, HasApiTokens;
+
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
